dc1020cd206cb798ac7da08cacceeecb6d977627
guna1202 <guna120202@gmail.com>
2023-12-09 22:15:26 +0530

    ReverseTheBit for all byte size

--- ReversingTheBits/ReversingTheBits.java
+++ ReversingTheBits/ReversingTheBits.java
@@ -3,20 +3,36 @@ import java.util.Scanner;
 public class ReversingTheBits {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    long a = 12;
+    long a = 11;
     long reversedDigits = reverseBit(a);
     System.out.println(reversedDigits);
   }
 
   private static long reverseBit(long a) {
-    long reverseNumber = 0;
+    long reversedBit = 0;
+    int first = 0;
+    int last = 0;
 
-    for (int i = 0; i < 32; i++) {
-      if ((a & (1L << i)) != 0) {
-        reverseNumber |= (1L << 31-i);
+    for (int i = 31; i >= 0; i--) {
+      if ((a & (1 << i)) > 0) {
+        first = i;
+        break;
       }
     }
+
+    while (first >= last) {
+      if ((a & (1 << first)) > 0) {
+        reversedBit = reversedBit | (1 << last);
+      }
+      
+      if ((a & (1 << last)) > 0) {
+        reversedBit = reversedBit | (1 << first);
+      }
+
+      first--;
+      last++;
+    }
     
-    return reverseNumber;
+    return reversedBit;
   }
 }
