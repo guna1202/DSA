69676a0641c914414fd782d2aac6e1f1a5b6efa6
guna1202 <guna120202@gmail.com>
2023-12-22 14:18:41 +0530

    Consecutive 1s Not Allowed with Space Optimised

--- Consecutive1sNotAllowed/Consecutive1sNotAllowed.java
+++ Consecutive1sNotAllowed/Consecutive1sNotAllowed.java
@@ -9,19 +9,18 @@ public class Consecutive1sNotAllowed {
 
   public static long countStrings(int n) {
     // code here
-    long arr0[] = new long[n + 1];
-    long arr1[] = new long[n + 1];
+    int mod = (int) 1e9 + 7;
+        
+        long fillZero = 1;
+        long fillOne = 1;
+        long total = 2;
     
-    int mod = (int)1e9 + 7;
-    
-    arr0[0] = arr1[0] = 0;
-    arr0[1] = arr1[1] = 1;
-    
-    for(int i = 2; i <= n; i++){
-        arr0[i] = arr0[i - 1] + arr1[i - 1] % mod;
-        arr1[i] = arr0[i - 1];
-    }
-    
-    return (arr0[n] + arr1[n]) % mod;
+        for (int i = 2; i <= n; i++) {
+            fillOne = fillZero;
+            fillZero = total;
+            total = (fillOne + fillZero) % mod;
+        }
+        
+        return total;
  }
 }
